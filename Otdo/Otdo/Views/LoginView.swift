29de1692f2395114e803a986b03240fbb5c169f0
FrediOS-dev <114239407+FrediOS-dev@users.noreply.github.com>
2022-12-19 17:36:15 +0900

    add loginView

--- Otdo/Otdo/Views/LoginView.swift
+++ Otdo/Otdo/Views/LoginView.swift
@@ -8,8 +8,113 @@
 import SwiftUI
 
 struct LoginView: View {
+    @State private var email: String = ""
+    @State private var password: String = ""
+    
+    @State private var secureField: Bool = true
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            VStack{
+                Spacer()
+                VStack {
+                    Text("OTDO")
+                        .font(.largeTitle)
+                        .fontWeight(.heavy)
+                        .italic()
+                    Text("온도별 옷차림")
+                        .fontWeight(.light)
+                }
+                Spacer()
+                TextField(" 이메일을 입력해주세요", text: $email)
+                    .modifier(TextFieldModifier())
+                    .padding(.vertical, 5)
+                
+                HStack{
+                    if secureField {
+                        SecureField(" 비밀번호를 입력해주세요", text: $password)
+                            .frame(height: 50)
+                    } else {
+                        TextField(" 비밀번호를 입력해주세요", text: $password)
+                            .frame(height: 50)
+                    }
+                    Button(action: {
+                        secureField.toggle()
+                    }) {
+                        Image(systemName: secureField ? "eye" : "eye.slash")
+                            .font(.footnote)
+                            .foregroundColor(.gray)
+                    }
+                }
+                .modifier(TextFieldModifier())
+                
+                HStack{
+                    // 틀렸을시 글씨색 빨간색으로
+                    Text("이메일 또는 비밀번호를 다시 확인하세요.")
+                        .font(.footnote)
+                        .padding(.trailing, 10)
+                        .foregroundColor(.white)
+                    NavigationLink(destination: Text("비밀번호 찾기")) {
+                        Text("비밀번호를 잊으셨나요?")
+                            .font(.footnote)
+                        
+                    }
+                }
+                Spacer()
+                
+                VStack{
+                    Button(action: {
+                        
+                    }){
+                        Text("로그인")
+                            .foregroundColor(.white)
+                            .bold()
+                            .background {
+                                RoundedRectangle(cornerRadius: 10, style: .continuous)
+                                    .frame(width: 360, height: 50)
+                                    .foregroundColor(.black)
+                            }
+                    }
+                    .padding(.vertical)
+                    
+                    Button(action: {
+                        
+                    }){
+                        Text("구글 로그인")
+                            .foregroundColor(.black)
+                            .bold()
+                            .frame(width: 328, height: 48)
+                            .padding(.horizontal, 15)
+                            .background {
+                                RoundedRectangle(cornerRadius: 10, style: .continuous)
+                                    .stroke(.gray, lineWidth: 2)
+                            }
+                    }
+                   
+                }
+                Spacer()
+                HStack{
+                    Text("계정이 없으신가요?")
+                        .font(.footnote)
+                    NavigationLink(destination: Text("회원가입 뷰")) {
+                        Text("회원가입")
+                            .font(.footnote)
+                    }
+                }
+            }
+            .padding()
+        }
+    }
+}
+
+struct TextFieldModifier: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+        .frame(height: 50)
+        .padding(.horizontal, 15)
+        .background {
+            RoundedRectangle(cornerRadius: 10, style: .continuous)
+                .stroke(.gray, lineWidth: 2)
+        }
     }
 }
 
