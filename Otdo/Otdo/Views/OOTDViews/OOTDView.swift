8b87113adc08cf651a783e573244f9256a9d3e2d
FrediOS-dev <114239407+FrediOS-dev@users.noreply.github.com>
2022-12-23 17:24:27 +0900

    feat: search post

--- Otdo/Otdo/Views/OOTDViews/OOTDView.swift
+++ Otdo/Otdo/Views/OOTDViews/OOTDView.swift
@@ -11,52 +11,75 @@ struct OOTDView: View {
     @EnvironmentObject var postStore: PostStore
     @EnvironmentObject var userInfoStore: UserInfoStore
     
-    let columns: [GridItem] = [
-        GridItem(.flexible(), spacing: 0, alignment: nil),
-        GridItem(.flexible(), spacing: 0, alignment: nil),
-        GridItem(.flexible(), spacing: 0, alignment: nil)
-    ]
+    //    let columns: [GridItem] = [
+    //        GridItem(.flexible(), spacing: 0, alignment: nil),
+    //        GridItem(.flexible(), spacing: 0, alignment: nil),
+    //        GridItem(.flexible(), spacing: 0, alignment: nil)
+    //    ]
     @State private var searchText: String = ""
     @State var isShowingAdd: Bool = false
-
+    
     
     var body: some View {
         NavigationStack{
             VStack{
                 HStack {
-                    Image(systemName: "magnifyingglass")
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                        .frame(width: 15, height: 15)
-                        .foregroundColor(.gray)
-                        .padding(15)
+                    
                     TextField("사용자 검색", text: $searchText)
+                        .padding(.leading)
+                        .onSubmit{
+                            postStore.fetchPostByUser(nickName: searchText)
+                        }
+                    Button(action: {
+                        if !searchText.isEmpty {
+                            postStore.fetchPostByUser(nickName: searchText)
+                        } else {
+                            postStore.fetchPost()
+                        }
+                    }) {
+                        Image(systemName: "magnifyingglass")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 15, height: 15)
+                            .foregroundColor(.gray)
+                            .padding(15)
+                    }
+                
                 }
                 .background(Color("SearchBar"))
                 .cornerRadius(10)
                 .padding(.horizontal, 10)
-                
+                //
                 TemperatureSliderView()
                 
                 ScrollView(showsIndicators: false) {
-                    LazyVGrid(
-                        columns: columns,
-                        alignment: .center,
-                        spacing: 5,
-                        pinnedViews: [],
-                        content:  {
-                            ForEach(Array(postStore.posts.enumerated()), id: \.offset) { (index, post) in
-                                NavigationLink(destination: PostDetailView(post: post, index: index)) {
-                                    
-                                    OOTDPostView(post: post)
+                    //                    ForEach(Array(postStore.posts)) { post in
+                    //                        if post.nickName == searchText {
+                    //                            SearchOOTDView(searchText: searchText, post: post).environment(postStore)
+                    //                        } else {
+                    //                            PostGridView()
+                    //                        }
+                    //
+                    //                    }
+                    GeometryReader{ bound in
+                        LazyVGrid(
+                            columns: [GridItem(.adaptive(minimum: bound.size.width / 3 - 1.2), spacing: 1.2)],
+                            alignment: .leading,
+                            spacing: 5,
+                            pinnedViews: [],
+                            content:  {
+                                ForEach(Array(postStore.posts.enumerated()), id: \.offset) { (index, post) in
+                                    NavigationLink(destination: PostDetailView(post: post, index: index)) {
+                                        OOTDPostView(post: post)
+                                    }
+                                    .foregroundColor(.black).environmentObject(postStore).environmentObject(userInfoStore)
                                 }
-                                .foregroundColor(.black).environmentObject(postStore).environmentObject(userInfoStore)
                             }
-                        }
-                    )
+                        )
+                    }
+                    .padding(.horizontal, 10)
+                    .padding(.vertical, 5)
                 }
-                .padding(.horizontal, 10)
-                .padding(.vertical, 5)
             }
             .toolbar{
                 ToolbarItem {
@@ -82,14 +105,15 @@ struct OOTDView: View {
             for post in postStore.posts {
                 postStore.retrievePhotos(post)
             }
-//            print(postStore.posts)
-        
+            //            print(postStore.posts)
+            
             print("=======================")
         }
+        
     }
 }
-    
-    
+
+
 //    struct OOTDView_Previews: PreviewProvider {
 //        static var previews: some View {
 //            OOTDView()
